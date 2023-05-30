f5db419df999055bd83d9eadd4b13b97c1cfb399
Manzoor Hussain <manzoor2015@namal.edu.pk>
2023-05-30 20:35:43 +0500

    Update Ashpy.py

--- pdf/user/services/Ashpy.py
+++ pdf/user/services/Ashpy.py
@@ -101,12 +101,11 @@ def ashbys_converter(path, pathout, path_save):
     "Interests" : ["interest1", "interest2", ...]
     }
     
-    Please keep the following points in considration while extracting data from text:
-        1. Do not summarize or rephrase Responsibilities. Extract each Responsibility completely from text.
+    You must keep the following points in considration while extracting data from text:
+        1. Do NOT split, rephrase or summarize list of Responsibilities. Extract each Responsibility as a complete sentence from text.
         2. Make it sure to keep the response in JSON format.
         3. If value not found then leave it empty/blank.
         4. Do not include Mobile number, Email and Home address.
-        5. Do not include Grade  
     """
 
     result = get_completion(test_text)
