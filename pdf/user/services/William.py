c33bec2ef62d96835c07b2ddd84073ce9dbe8527
Manzoor Hussain <manzoor2015@namal.edu.pk>
2023-05-30 20:41:20 +0500

    Update William.py

--- pdf/user/services/William.py
+++ pdf/user/services/William.py
@@ -104,12 +104,11 @@ def william_blake_converter(path, formatted_path,save_path):
     "Interests":["interests1","interests2",...]
     }
 
-    Please keep the following points in considration while extracting data from text:
-        1. Do not summarize or rephrase Responsibilities. Extract each Responsibility completely from text.
+    You must keep the following points in considration while extracting data from text:
+        1. Do NOT split, rephrase or summarize list of Responsibilities. Extract each Responsibility as a complete sentence from text.
         2. Make it sure to keep the response in JSON format.
         3. If value not found then leave it empty/blank.
         4. Do not include Mobile number, Email and Home address.
-        5. Do not include Grade
     """
 
 
