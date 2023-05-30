b4bdfbbf72c8698ac2a2cf1891979c362df20530
Manzoor Hussain <manzoor2015@namal.edu.pk>
2023-05-30 20:40:53 +0500

    Update Timber.py

--- pdf/user/services/Timber.py
+++ pdf/user/services/Timber.py
@@ -92,12 +92,11 @@ def timber_seed_format_converter(path, pathout, path_save):
     "Interests" : ["interest1", "interest2", ...],
     }
 
-    Please keep the following points in considration while extracting data from text:
-        1. Do not summarize or rephrase Responsibilities. Extract each Responsibility completely from text.
+    You must keep the following points in considration while extracting data from text:
+        1. Do NOT split, rephrase or summarize list of Responsibilities. Extract each Responsibility as a complete sentence from text.
         2. Make it sure to keep the response in JSON format.
         3. If value not found then leave it empty/blank.
         4. Do not include Mobile number, Email and Home address.
-        5. Do not include Grade
     """
 
 
