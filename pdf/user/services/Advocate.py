25ce61165fa62447ee51f0480ae0072a304960ec
Manzoor Hussain <manzoor2015@namal.edu.pk>
2023-06-09 11:55:40 +0500

    Update Advocate.py

--- pdf/user/services/Advocate.py
+++ pdf/user/services/Advocate.py
@@ -10,6 +10,7 @@ from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 from docx.shared import Pt
 import PyPDF2
 import pdfplumber
+import traceback
 
 
 def get_completion(prompt, model="gpt-3.5-turbo"):
@@ -23,11 +24,7 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
 
 def advocate_group_converter(path, pathout, path_save):
     formatted= pathout
-#     un_formatted=os.getcwd() + path
-    
-#     doc = docx.Document(un_formatted)
-#     formated_text = docx2txt.process(formatted)
-#     unformated_text = docx2txt.process(un_formatted)
+
     def read_text_from_docx(path):
         doc = docx.Document(path)
         text = [paragraph.text for paragraph in doc.paragraphs]
@@ -51,8 +48,6 @@ def advocate_group_converter(path, pathout, path_save):
 
     os.environ["OPEN_API_KEY"] = api_key
     openai.api_key = api_key
-    # llm=OpenAI(temperature=0, max_tokens=1500,openai_api_key=api_key)
-    fields_labels = "Name, PROFILE, EDUCATION, IT LITERACY, CERTIFICATES, PROJECTS, PROFESSIONAL QUALIFICATIONS, SOFTWARES, languages, Interests, TRAININGS, skills, WORK EXPERIENCE"
 
     
     print ("Process has Started...")
@@ -69,56 +64,59 @@ def advocate_group_converter(path, pathout, path_save):
 
     "Experience" : [
         {"Company Name" : "Name of company",
-        "Company location": "Location of that company",
-        "Duration" : "Working duration in company",
-        "Designation" : "Specific designation in that Company",
-        "Responsibilities" : ["Responsibility 1", "Responsibility 2", ...]
+         "Duration" : "Working Duration in Company",
+         "Designation" : "Specific designation in that Company",
+         "Responsibilities" : ["Responsibility1", "Responsibility2", ...],
         },
 
         {"Company Name" : "Name of company",
-        "Company location": "Location of that company",
-        "Duration" : "Working duration in company",
-        "Designation" : "Specific designation in that Company",
-        "Responsibilities" : ["Responsibility 1", "Responsibility 2", ...]
+         "Duration" : "Working Duration in Company",
+         "Designation" : "Specific designation in that Company",
+         "Responsibilities" : ["Responsibility1", "Responsibility2", ...],
         },
         ...
         ]
     "Education" : [
         {"Institute Name":"Name of that institute",
-          "Institute location":"Location of that institute",
-           "Degree":"Name of that degree",
-           "Duration":"duration of that degree,
+         "Institute location":"Location of that institute",
+         "Degree":"Name of that degree",
+         "Duration":""Studying duration in institute",
         },
         {"Institute Name":"Name of that institute",
-          "Institute location":"Location of that institute",
-           "Degree":"Name of that degree",
-           "Duration":"duration of that degree,
+         "Institute location":"Location of that institute",
+         "Degree":"Name of that degree",
+         "Duration":"Studying duration in institute.",
         },
     ...],
     "Training" : ["training1", "training2", ...],
     "Skills" : ["skill1", "skill2", ...],
     "Qualification" : ["qualification1", "qualification2", ...],
     "Languages" : ["language1", "language2", ...],
-    "Interests" : ["interest1", "interest2", ...]
+    "Interests" : ["interest1", "interest2", ...],
     }
 
+   You must keep the following points in considration while extracting data from text:
     1. Do NOT split, rephrase or summarize list of Responsibilities. Extract each Responsibility as a complete sentence from text.
     2. Make it sure to keep the response in JSON format.
     3. If value not found then leave it empty/blank.
     4. Do not include Mobile number, Email and Home address.
+    5. Summary/Personal Statement should be complete without being rephrased.
     """
 
 
     result = get_completion(test_text)
 
     dc = dict(json.loads(re.sub(r'\[\"\"\]',r'[]',re.sub(r'\"[Un]nknown\"|\"[Nn]one\"|\"[Nn]ull\"|\"[Nn]ot [Mm]entioned\"',r'""',re.sub(r',[ \n]*\]',r']',re.sub(r',[ \n]*\}',r'}',result.replace('...','')))))))
+#     print(dc)
+
 
     doc = docx.Document(formatted)
 
     for i,p in enumerate(doc.paragraphs):
         try:
             if p.text.strip().lower() == 'name':
-                    doc.paragraphs[i].text = ""
+                doc.paragraphs[i].text = ""
+                if dc['Name'].lower().replace(' ','')!='value':
                     run = doc.paragraphs[i].add_run(dc['Name'].strip().title())
                     run.bold = True
                     run.font.size = Pt(16.5)
@@ -126,74 +124,102 @@ def advocate_group_converter(path, pathout, path_save):
             pass
         try:
             if p.text.strip(' :\n').lower() == 'summary':
-                doc.paragraphs[i+2].add_run(dc['Summary'].strip()).bold = False
-                doc.paragraphs[i+2].alignment = WD_PARAGRAPH_ALIGNMENT.JUSTIFY
+                if dc['Summary'].lower().replace(' ','') != 'value':
+                    doc.paragraphs[i+2].add_run(dc['Summary'].strip()).bold = False
+                    doc.paragraphs[i+2].alignment = WD_PARAGRAPH_ALIGNMENT.JUSTIFY
 
         except:
             pass
 
         if p.text.strip(' :\n').lower() == 'education':
             for j in dc['Education']:
-                doc.paragraphs[i+2].add_run(j['Degree'].strip() + "\n").bold=True
-                doc.paragraphs[i+2].add_run(j['Institute Name'].strip() + "\n").bold=False
-                doc.paragraphs[i+2].add_run(j['Institute location'].strip() + "\n").bold=False
-                doc.paragraphs[i+2].add_run(j['Duration'].strip() + "\n").bold=True
-
-    #             doc.paragraphs[i+2].add_run(j['Thesis'].strip() + "\n\n").bold=False
-
-    #               + j['Institute'].strip() + "–" + j['Duration'].strip()).bold = False
-    #               doc.paragraphs[i+2].add_run('  • ' + j.strip() + '\n').bold = False
-
-
+                degree=j['Degree'].strip()
+                institute=j['Institute Name'].strip()
+                duration=j['Duration'].strip()
+                if degree and degree.lower().replace(' ','') != "nameofthatdegree":
+                    if institute:
+                        doc.paragraphs[i+2].add_run(institute).bold=False
+                    else:
+                        doc.paragraphs[i+2].add_run("Institute not mentioned").bold=False
+                        
+                    doc.paragraphs[i+2].add_run(" - "+degree + "\n").bold=False
+                    
+                    if duration:
+                        doc.paragraphs[i+2].add_run(duration + "\n\n").bold=False
+                    else:
+                        doc.paragraphs[i+2].add_run("Duration not mentioned" + "\n\n").bold=False
 
         try:
             if p.text.strip(' :\n').lower() == 'qualification':
-                for j in dc['Qualification']:
-                    doc.paragraphs[i+2].add_run('  • ' + j.strip() + '\n').bold = False
+                if dc['Qualification'][0].lower().replace(' ','') != 'qualification1':
+                    for j in dc['Qualification']:
+                        if j.strip():
+                            doc.paragraphs[i+2].add_run('  • ' + j.strip() + '\n').bold = False
         except:
             pass
 
         try:
             if p.text.strip(' :\n').lower() == 'languages':
-                for j in dc['Languages']:
-                    doc.paragraphs[i+2].add_run('  • ' + j.strip() + '\n').bold = False
+                if dc['Languages'][0].lower().replace(' ','') != 'languages1':
+                    for j in dc['Languages']:
+                        if j.strip():
+                            doc.paragraphs[i+2].add_run('  • ' + j.strip() + '\n').bold = False
         except:
             pass
 
         try:
             if p.text.strip(' :\n').lower() == 'interests':
-                for j in dc['Interests']:
-                    doc.paragraphs[i+2].add_run('  • ' + j.strip() + '\n').bold = False
+                if dc['Interests'][0].lower().replace(' ','') != 'interests1':
+                    for j in dc['Interests']:
+                        if j.strip():
+                            doc.paragraphs[i+2].add_run('  • ' + j.strip() + '\n').bold = False
         except:
             pass
 
         try:
             if p.text.strip(' :\n').lower() == 'training':
-                for j in dc['Training']:
-                    doc.paragraphs[i+2].add_run('  • ' + j.strip() + '\n').bold = False
+                if dc['Training'][0].lower().replace(' ','') != 'training1':
+                    for j in dc['Training']:
+                        if j.strip():
+                            doc.paragraphs[i+2].add_run('  • ' + j.strip() + '\n').bold = False
         except:
             pass
 
         try:
             if p.text.strip(' :\n').lower() == 'skills':
-                for j in dc['Skills']:
-                    doc.paragraphs[i+2].add_run('  • ' + j.strip() + '\n').bold = False
+                if dc['Skills'][0].lower().replace(' ','') != 'skills1':
+                    for j in dc['Skills']:
+                        if j.strip():
+                            doc.paragraphs[i+2].add_run('  • ' + j.strip() + '\n').bold = False
         except:
             pass
 
         try:
             if p.text.strip(' :\n').lower() == 'experience':
                 for j in dc['Experience']:
-                    doc.paragraphs[i+2].add_run(j['Company Name'].strip() + '\n').bold = True
-                    doc.paragraphs[i+2].add_run(j['Designation'].strip() + '\n').bold = True
-                    doc.paragraphs[i+2].add_run(j['Duration'].strip() + '\n').bold = True
+                    company_name = j['Company Name'].strip()
+                    duration = j['Duration'].strip()
+                    designation = j['Designation'].strip()
+                    if designation and designation.lower().replace(' ','') !='specificdesignationinthatcompany' or (company_name and company_name.lower().replace(' ','') !='nameofcompany'):                        
+                        if designation:
+                            doc.paragraphs[i+2].add_run(designation + '\n').bold = True
+                        else:                       
+                            doc.paragraphs[i+2].add_run("Designation not mentioned").bold=False
 
-    #                 doc.paragraphs[i+2].add_run('Responsibilities:' + '\n').bold = True
-                    if j['Responsibilities']:
-                        doc.paragraphs[i+2].add_run('\n')
-                    for k in j['Responsibilities']:
-                        doc.paragraphs[i+2].add_run('  • ' + k.strip() + '\n').bold = False
-                    doc.paragraphs[i+2].add_run('\n\n')
+                        if company_name:
+                            doc.paragraphs[i+2].add_run(company_name+'\n').bold=True
+                        else:
+                            doc.paragraphs[i+2].add_run("Company Name not mentioned").bold=False
+
+                        if duration:
+                            doc.paragraphs[i+2].add_run(duration + "\n\n").bold=True
+                        else:
+                            doc.paragraphs[i+2].add_run("Duration not mentioned" + "\n\n").bold=False   
+
+                        if j['Responsibilities'] and j['Responsibilities'][0].lower().replace(' ','') != 'responsibility1':
+                            for k in j['Responsibilities']:
+                                doc.paragraphs[i+2].add_run('  • ' + k.strip() + '\n').bold = False
+                            doc.paragraphs[i+2].add_run('\n\n')
         except:
             pass
 
