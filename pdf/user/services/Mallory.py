f8a33f0b44740c168bdbedcf730618fabf3be3ec
Manzoor Hussain <manzoor2015@namal.edu.pk>
2023-06-09 13:00:00 +0500

    Update Mallory.py

--- pdf/user/services/Mallory.py
+++ pdf/user/services/Mallory.py
@@ -9,7 +9,6 @@ from docx.enum.text import WD_UNDERLINE
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 from docx.shared import Pt
 import PyPDF2
-import pdfplumber
 from docx.shared import Pt
 from docx.shared import RGBColor
 
@@ -53,8 +52,6 @@ def mallory_converter(path ,formatted_path,save_path):
 
     os.environ["OPEN_API_KEY"] = api_key
     openai.api_key = api_key
-    # llm=OpenAI(temperature=0, max_tokens=1500,openai_api_key=api_key)
-    fields_labels = "Name, PROFILE, EDUCATION, IT LITERACY, CERTIFICATES, PROJECTS, PROFESSIONAL QUALIFICATIONS, SOFTWARES, languages, Interests, TRAININGS, skills, WORK EXPERIENCE"
 
     
     print ("Process has Started...")
@@ -68,29 +65,28 @@ def mallory_converter(path ,formatted_path,save_path):
 {
 "Name":"candidate name",
 "Profile" : "value",
-
 "Employment History" : [
     {"Company Name" : "Name of company",
      "Duration" : "Working duration in company",
      "Designation" : "Specific designation in that Company",
-     "Responsibilities" : ["Responsibility 1", "Responsibility 2", ...]
+     "Responsibilities" : ["Responsibility 1", "Responsibility 2", ...],
     },
     
     {"Company Name" : "Name of company",
      "Duration" : "Working duration in company",
      "Designation" : "Specific designation in that Company",
-     "Responsibilities" : ["Responsibility 1", "Responsibility 2", ...]
+     "Responsibilities" : ["Responsibility 1", "Responsibility 2", ...],
     },
     ...
     ]
 "Education" : [
     {"Institute Name":"Name of that institute",
-     "Duration":"duration of that degree,
+     "Duration":"duration of that degree",
      "Degree":"Name of that degree",
     },
     
     {"Institute Name":"Name of that institute",
-     "Duration":"duration of that degree,
+     "Duration":"duration of that degree",
      "Degree":"Name of that degree",
     },
    ...],
@@ -98,14 +94,17 @@ def mallory_converter(path ,formatted_path,save_path):
 "Skills" : ["skills1", "skills2", ...],
 "Qualifications" : ["qualifications1", "qualifications2", ...],
 "Language":["language1","language2",...],
-"Interests":["interests1","interests2",...]
+"Interests":["interests1","interests2",...],
 
 }
 
-     1. Do NOT split, rephrase or summarize list of Responsibilities. Extract each Responsibility as a complete sentence from text.
-     2. Make it sure to keep the response in JSON format.
-     3. If value not found then leave it empty/blank.
-     4. Do not include Mobile number, Email and Home address.
+   You must keep the following points in considration while extracting data from text:
+    1. Do NOT split, rephrase or summarize list of Responsibilities. Extract each Responsibility as a complete sentence from text.
+    2. Make it sure to keep the response in JSON format.
+    3. If value not found then leave it empty/blank.
+    4. Do not include Mobile number, Email and Home address.
+    5. Summary/Personal Statement should be complete without being rephrased.
+
 
     """
 
@@ -113,7 +112,7 @@ def mallory_converter(path ,formatted_path,save_path):
     result = get_completion(test_text)
 #     print(result)
 
-    dc = dict(json.loads(re.sub(r'\[\"\"\]',r'[]',re.sub(r'\"[Un]nknown\"|\"[Nn]one\"|\"[Nn]ull\"',r'""',re.sub(r',[ \n]*\]',r']',re.sub(r',[ \n]*\}',r'}',result.replace('...','')))))))
+    dc = dict(json.loads(re.sub(r'\[\"\"\]',r'[]',re.sub(r'\"[Un]nknown\"|\"[Nn]one\"|\"[Nn]ull\"|\"[Nn]ot [Mm]entioned\"',r'""',re.sub(r',[ \n]*\]',r']',re.sub(r',[ \n]*\}',r'}',result.replace('...','')))))))
 #     print(dc)
 
     doc = docx.Document(formatted)
@@ -121,42 +120,45 @@ def mallory_converter(path ,formatted_path,save_path):
     for i,p in enumerate(doc.paragraphs):
         try:
             if p.text.strip().lower() == 'name':
-                    doc.paragraphs[i].text = ""
+                doc.paragraphs[i].text = ""
+                if dc['Name'].lower().replace(' ','')!='candidatename':
                     run = doc.paragraphs[i].add_run(dc['Name'].strip().title())
                     run.bold = True
                     run.font.color.rgb = RGBColor(72,0,0)  # Red color
                     run.font.size = Pt(23.5)
         except:
             pass
-    #     try:
-    #         if p.text.strip().lower() == 'location':
-    #                 doc.paragraphs[i].text = ""
-    #                 run = doc.paragraphs[i].add_run(dc['Location'].strip().title())
-    #                 run.bold = True
-    # #                 run.font.size = Pt(16.5)
-    #     except:
-    #         pass
+
         try:
             if p.text.strip(' :\n').lower() == 'profile':
-    #             doc.paragraphs[i].text = ""
-                run1=doc.paragraphs[i+2].add_run(dc['Profile'].strip())
-                run1.bold=False
-                run1=doc.paragraphs[i+2].alignment = WD_PARAGRAPH_ALIGNMENT.JUSTIFY
+                if dc['Profile'].lower().replace(' ','') != 'value':
+                    run1=doc.paragraphs[i+2].add_run(dc['Profile'].strip())
+                    run1.bold=False
+                    run1=doc.paragraphs[i+2].alignment = WD_PARAGRAPH_ALIGNMENT.JUSTIFY
 
         except:
-            pass
+                pass
 
         if p.text.strip(' :\n').lower() == 'education':
             for j in dc['Education']:
-                run1 = doc.paragraphs[i+2].add_run(j['Institute Name'].strip()+"\n")
-                run1.font.color.rgb = RGBColor(145, 148, 146)  # Red color
-                run2 = doc.paragraphs[i+2].add_run(j['Duration'].strip()+"\n")
-                run2.bold=False
-                run2.font.color.rgb = RGBColor(145, 148, 146)  # Red color
-                run3 = doc.paragraphs[i+2].add_run(j['Degree'].strip()+"\n\n")
-                run3.bold=False
-                run3.font.color.rgb = RGBColor(145, 148, 146)  # Red color
+                if j['Degree'].strip() and j['Degree'].lower().replace(' ','') != "nameofthatdegree":
+                    run3 = doc.paragraphs[i+2].add_run(j['Degree'].strip()+"\n")
+                    run3.bold=False
+                    run3.font.color.rgb = RGBColor(145, 148, 146)  # Red color
 
+                    if j['Institute Name'].strip():
+                        run1 = doc.paragraphs[i+2].add_run(j['Institute Name'].strip()+"\n")
+                        run1.font.color.rgb = RGBColor(145, 148, 146)  # Red color
+                    else:
+                         doc.paragraphs[i+2].add_run("Institute not mentioned").bold=False
+                    if j['Duration'].strip():
+                        run2 = doc.paragraphs[i+2].add_run(j['Duration'].strip()+"\n\n")
+                        run2.bold=False
+                        run2.font.color.rgb = RGBColor(145, 148, 146)  # Red color
+                    else:
+                        doc.paragraphs[i+2].add_run("Duration not mentioned" + "\n").bold=False
+          
+                               
     #             run4.italic=True
 
     #             doc.paragraphs[i+2].add_run(j['Duration'].strip()+"\n").bold=False
@@ -171,66 +173,83 @@ def mallory_converter(path ,formatted_path,save_path):
 
         try:
             if p.text.strip(' :\n').lower() == 'certifications':
-                for j in dc['Certifications']:
-                    doc.paragraphs[i+2].add_run('  • ' + j.strip() + '\n').bold = False
+                if dc['Certifications'][0].lower().replace(' ','') != 'certifications1':
+                    for j in dc['Certifications']:
+                        if j.strip():
+                            doc.paragraphs[i+2].add_run('  • ' + j.strip() + '\n').bold = False
         except:
             pass
 
         try:
             if p.text.strip(' :\n').lower() == 'qualifications':
-                for j in dc['Qualifications']:
-                    doc.paragraphs[i+2].add_run('  • ' + j.strip() + '\n').bold = False
+                if dc['Qualifications'][0].lower().replace(' ','') != 'qualifications1':
+                    for j in dc['Qualifications']:
+                        if j.strip():
+                            doc.paragraphs[i+2].add_run('  • ' + j.strip() + '\n').bold = False
         except:
             pass
 
         try:
             if p.text.strip(' :\n').lower() == 'language':
-                for j in dc['Language']:
-                    doc.paragraphs[i+2].add_run('  • ' + j.strip() + '\n').bold = False
+                if dc['Language'][0].lower().replace(' ','') != 'language1':
+                    for j in dc['Language']:
+                        if j.strip():
+                            doc.paragraphs[i+2].add_run('  • ' + j.strip() + '\n').bold = False
         except:
             pass
 
         try:
             if p.text.strip(' :\n').lower() == 'interests':
-                for j in dc['Interests']:
-                    doc.paragraphs[i+2].add_run('  • ' + j.strip() + '\n').bold = False
+                if dc['Interests'][0].lower().replace(' ','') != 'interests1':
+                    for j in dc['Interests']:
+                        if j.strip():
+                            doc.paragraphs[i+2].add_run('  • ' + j.strip() + '\n').bold = False
         except:
             pass
 
         try:
             if p.text.strip(' :\n').lower() == 'trainings':
-                for j in dc['Trainings']:
-                    doc.paragraphs[i+2].add_run('  • ' + j.strip() + '\n').bold = False
+                if dc['Training'][0].lower().replace(' ','') != 'training1':
+                    for j in dc['Trainings']:
+                        if j.strip():
+                            doc.paragraphs[i+2].add_run('  • ' + j.strip() + '\n').bold = False
         except:
             pass
 
         try:
             if p.text.strip(' :\n').lower() == 'skills':
-                for j in dc['Skills']:
-                    doc.paragraphs[i+2].add_run('  • ' + j.strip() + '\n').bold = False
+                if dc['Skills'][0].lower().replace(' ','')!= 'skills1':
+                    for j in dc['Skills']:
+                        if j.strip():
+                            doc.paragraphs[i+2].add_run('  • ' + j.strip() + '\n').bold = False
         except:
             pass
 
         try:
             if p.text.strip(' :\n').lower() == 'employment history':
-                for j in dc['Employment History']:
-                    run1=doc.paragraphs[i+2].add_run(j['Company Name'].strip()+"\n")
-                    run1.font.color.rgb = RGBColor(145, 148, 146)  # Red color
-                    run2=doc.paragraphs[i+2].add_run(j['Duration'].strip() + "\n")
-                    run2.font.color.rgb = RGBColor(145, 148, 146)  # Red color
-                    run3=doc.paragraphs[i+2].add_run(j['Designation'].strip() + "\n\n")
-                    run3.font.color.rgb = RGBColor(145, 148, 146)  # Red color
-                    run3.bold=False
-    #                 run3.italic=True
-    #                 run4=doc.paragraphs[i+2].add_run("\t\t\t\t\t\t"+j['Location'].strip()+"\n\n")
-    #                 run4.bold=False
-    #                 doc.paragraphs[i+2].add_run(j['Duration'].strip()+'\t\t'+j['Company Name'].strip()+ "\n").bold = True
-    #                 doc.paragraphs[i+2].add_run('Responsibilities:' + '\n').bold = True
-    #                 if j['Responsibilities']:
-    #                     doc.paragraphs[i+2].add_run('\n')
-                    for k in j['Responsibilities']:
-                        doc.paragraphs[i+2].add_run('  • ' + k.strip() + '\n').bold = False
-                    doc.paragraphs[i+2].add_run('\n\n')
+                for j in dc['Employment History']:                  
+                    if j['Designation'].strip() and j['Designation'].lower().replace(' ','') !='specificdesignationinthatcompany' or (j['Company Name'].strip() and j['Company Name'].lower().replace(' ','') !='nameofcompany'):                        
+                        if j['Designation'].strip():
+                            run3=doc.paragraphs[i+2].add_run(j['Designation'].strip() + "\n")
+                            run3.font.color.rgb = RGBColor(145, 148, 146)  # Red color
+                            run3.bold=False
+                        else:
+                            doc.paragraphs[i+2].add_run("Designation not mentioned\n").bold=False
+                        if j['Company Name'].strip():                                                   
+                            run1=doc.paragraphs[i+2].add_run(j['Company Name'].strip()+"\n")
+                            run1.font.color.rgb = RGBColor(145, 148, 146)  # Red color
+                        else:
+                            doc.paragraphs[i+2].add_run("Company Name not mentioned\n").bold=False
+                        if j['Duration'].strip():                          
+                            run2=doc.paragraphs[i+2].add_run(j['Duration'].strip() + "\n\n")
+                            run2.font.color.rgb = RGBColor(145, 148, 146)  # Red color
+                        else:
+                            doc.paragraphs[i+2].add_run("Duration not mentioned\n\n").bold=False
+                                
+                        if j['Responsibilities'] and j['Responsibilities'][0].lower().replace(' ','') != 'responsibility1':
+                            for k in j['Responsibilities']:
+                                doc.paragraphs[i+2].add_run('  • ' + k.strip() + '\n').bold = False
+                            doc.paragraphs[i+2].add_run('\n\n')
         except:
             pass
 
