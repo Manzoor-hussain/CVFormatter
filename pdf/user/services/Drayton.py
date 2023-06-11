1134966147226bef75e66773999c6b485fa5e907
Manzoor Hussain <manzoor2015@namal.edu.pk>
2023-06-12 02:44:56 +0500

    Update Drayton.py

--- pdf/user/services/Drayton.py
+++ pdf/user/services/Drayton.py
@@ -22,7 +22,7 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
     return response.choices[0].message["content"]
     
 
-def drayton_converter(path_in, path_out, path_save):
+def drayton_converter(path, path_out, path_save):
     
     formatted = path_out
     
